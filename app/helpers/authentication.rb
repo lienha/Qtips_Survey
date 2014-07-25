7668f9176ae637310d342eaf1b3631bae015c75b
quytx1102 <quytx1102@gmail.com>
2014-07-25 17:57:21 -0500

    changed css

--- app/helpers/authentication.rb
+++ app/helpers/authentication.rb
@@ -1,8 +1,13 @@
 helpers do
   def current_user
     if session[:user_id]
-      @user ||= User.find(session[:user_id])
+      begin
+        @user ||= User.find(session[:user_id])
+      rescue
+        @user = nil
+      end
     end
+  end
 
   def not_signed_in?
     if current_user.nil?
