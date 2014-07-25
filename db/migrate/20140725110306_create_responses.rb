6cf2257989909d577efe5850f747de93c29cfe8b
quytx1102 <quytx1102@gmail.com>
2014-07-25 16:19:39 -0500

    removed question association

--- db/migrate/20140725110306_create_responses.rb
+++ db/migrate/20140725110306_create_responses.rb
@@ -1,7 +1,6 @@
 class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
-      t.belongs_to :question
       t.belongs_to :answer
       t.belongs_to :sample
       t.timestamps
