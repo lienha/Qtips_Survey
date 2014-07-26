245fbaba55a44472c7c16e4cb1e647d5a2c5164c
jermspeaks <jermspeaks@gmail.com>
2014-07-25 19:42:01 -0500

    adding all surveys

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,4 +1,5 @@
 get '/surveys' do
+	@surveys = Survey.all
 	erb :"surveys/index"
 end
 
@@ -11,14 +12,17 @@ end
 get '/surveys/:survey_id/questions/:question_id' do
 	survey = params[:survey_id]
 	question = params[:question_id]
+	@question = Question.find(question)
+	@survey = Survey.find(survey)
+	@answers = @question.answers
 	erb :"surveys/show"
 end
 
 post '/surveys/:survey_id/questions/:question_id' do
 	survey = params[:survey_id]
 	question = params[:question_id]
-	if question_count(question)
-		next_question = question + 1
+	if !question_count(question)
+		next_question = question.to_i + 1
 		redirect "/surveys/#{survey}/questions/#{next_question}"
 	else
 		redirect '/surveys'
--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -1,7 +1,6 @@
-helpers do
-	def question_count(question_id)
-		q = Question.find(question_id)
-		last_id = q.survey.questions.last.id
-		question_id == last_id
-	end
+
+def question_count(question_id)
+	q = Question.find(question_id)
+	last_id = q.survey.questions.last.id
+	q.id != last_id
 end
